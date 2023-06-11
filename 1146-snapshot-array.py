c6e606c298d2d579c6eaf35c3fba7b5b2998cd75
Peter Rauscher <peterrauscher@protonmail.com>
2023-06-11 10:57:05 -0400

    Better solution for SnapshotArray, using Linked Lists. But TLE

--- 1146-snapshot-array.py
+++ 1146-snapshot-array.py
@@ -1,31 +1,79 @@
+class Node:
+    def __init__(self, n: int, next=None, snaps=1):
+        self.n = n
+        self.next = next
+        self.snaps = snaps
+
+
+class LinkedList:
+    def __init__(self, head):
+        self.head = head
+
+    def get(self, i: int) -> int:
+        at = 0
+        curr = self.head
+        while at <= i and curr.next is not None:
+            if (at + curr.snaps) > i:
+                return curr.n
+            at = at + curr.snaps
+            curr = curr.next
+        return curr.n
+
+    def insertIfDifferent(self, n: int) -> None:
+        curr = self.head
+        while curr.next is not None:
+            curr = curr.next
+        if curr.n == n:
+            curr.snaps += 1
+        else:
+            curr.next = Node(n)
+
+
 class SnapshotArray:
     def __init__(self, length: int):
+        self.heads = None
         self.data = [0 for _ in range(length)]
         self.snapCount = -1
-        self.snapshots = []
 
     def set(self, index: int, val: int) -> None:
         self.data[index] = val
 
     def snap(self) -> int:
-        self.snapshots.append(self.data.copy())
+        if self.heads == None:
+            self.heads = [LinkedList(Node(n)) for n in self.data]
+        else:
+            for i, ll in enumerate(self.heads):
+                ll.insertIfDifferent(self.data[i])
         self.snapCount += 1
-        if self.snapCount == 0:
-            return 0
-        return self.snapCount - 1
+        return self.snapCount
 
     def get(self, index: int, snap_id: int) -> int:
-        return self.snapshots[snap_id][index]
+        return self.heads[index].get(snap_id)
 
 
-snapshotArr = SnapshotArray(3)
-snapshotArr.set(0, 5)
-snapshotArr.snap()
-snapshotArr.set(0, 6)
-snapshotArr.get(0, 0)
+# ["SnapshotArray","snap","snap","set","snap","get","set","get","snap","get"]
+# [[1],[],[],[0,4],[],[0,1],[0,12],[0,1],[],[0,3]]
+# Expected: [null,0,1,null,2,0,null,0,3,12]
+events = [
+    "SnapshotArray",
+    "snap",
+    "snap",
+    "set",
+    "snap",
+    "get",
+    "set",
+    "get",
+    "snap",
+    "get",
+]
+datas = [[1], [], [], [0, 4], [], [0, 1], [0, 12], [0, 1], [], [0, 3]]
 
-# Your SnapshotArray object will be instantiated and called as such:
-# obj = SnapshotArray(length)
-# obj.set(index,val)
-# param_2 = obj.snap()
-# param_3 = obj.get(index,snap_id)
+sa = SnapshotArray(datas[0][0])
+for i in range(1, len(events)):
+    if events[i] == "snap":
+        print(sa.snap())
+    elif events[i] == "set":
+        sa.set(datas[i][0], datas[i][1])
+    elif events[i] == "get":
+        print(sa.get(datas[i][0], datas[i][1]))
+print("Done!")
