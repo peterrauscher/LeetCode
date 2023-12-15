3015357aba2e004a47523e45db06695480f6fc32
Peter Rauscher <peterrauscher@protonmail.com>
2023-12-15 12:43:06 -0500

    Time: 729 ms (85.46%) | Memory: 38.9 MB (40.56%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -2,9 +2,9 @@ from collections import deque
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted([(position[i], speed[i]) for i in range(len(position))], reverse=True)
+        position = sorted([(position[i], speed[i]) for i in range(len(position))], reverse=True)
         times_to_reach_target = deque()
-        for car in cars:
+        for car in position:
             travel_time = (target - car[0]) / car[1]
             if not times_to_reach_target or travel_time > times_to_reach_target[-1]:
                 times_to_reach_target.append(travel_time)
