21c74fcc49a989972736dc13a1f688c444eb77dc
Peter Rauscher <peterrauscher@protonmail.com>
2024-02-19 15:55:11 -0500

    Time: 43 ms (29.53%) | Memory: 17 MB (51.26%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -4,18 +4,16 @@ class Solution:
             return False
         return dividend.replace(divisor, "") == ""
 
-    def gcdHelper(self, str1: str, str2: str, l: int, tried):
+    def gcdHelper(self, str1: str, str2: str, l: int):
         if l == len(str2):
             return ""
         gcd = str2[l:]
-        if gcd not in tried:
-            if self.divides(gcd, str1) and self.divides(gcd, str2):
-                return gcd
-            tried.add(gcd)
-        return self.gcdHelper(str1, str2, l + 1, tried)
+        if self.divides(gcd, str1) and self.divides(gcd, str2):
+            return gcd
+        return self.gcdHelper(str1, str2, l + 1)
 
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if len(str1) < len(str2):
-            return self.gcdHelper(str2, str1, 0, set())
+            return self.gcdHelper(str2, str1, 0)
         else:
-            return self.gcdHelper(str1, str2, 0, set())
+            return self.gcdHelper(str1, str2, 0)
