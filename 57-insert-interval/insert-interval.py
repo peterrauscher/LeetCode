7fcb368e0249b0548da221d0cc5d348c030161b5
Peter Rauscher <peterrauscher@protonmail.com>
2024-02-19 11:50:09 -0500

    Time: 79 ms (42.82%) | Memory: 20 MB (68.56%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -2,24 +2,12 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         # Start with a binary search to find sorted position in array
         # 
-        if len(intervals) == 0: return [newInterval]
-        newIntervals = []
-        i = 0
-        while i < len(intervals):
-            if newInterval[0] > intervals[i][1]:
-                newIntervals.append(intervals[i])
-            elif newInterval[0] <= intervals[i][1]:
-                if newInterval[1] < intervals[i][0]:
-                    newIntervals.append(newInterval)
-                    newIntervals += intervals[i:]
-                    return newIntervals
-                thisNewInterval = [min(intervals[i][0], newInterval[0])]
-                while i < len(intervals) and newInterval[1] >= intervals[i][0]:
-                    i += 1
-                thisNewInterval.append(max(newInterval[1], intervals[i-1][1]))
-                newIntervals.append(thisNewInterval)
-                newIntervals += intervals[i:]
-                return newIntervals
-            i += 1
-        newIntervals.append(newInterval)
-        return newIntervals
+        left, right = [], []
+        for interval in intervals:
+            if newInterval[0] > interval[1]:
+                left.append(interval)
+            elif newInterval[1] < interval[0]:
+                right.append(interval)
+            else:
+                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
+        return left + [newInterval] + right
